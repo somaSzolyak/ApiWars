47fcabf00500af820c1a7e3df64bf16d26f7b96f
Soma Szolyák <soma.szolyak@gmail.com>
2018-04-23 10:38:48 +0200

    pom and src

---
+++ src/main/java/com/codecool/apiwars/WarApp.java
@@ -0,0 +1,11 @@
+package com.codecool.apiwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WarApp.class, args);
+    }
+}
