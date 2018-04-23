e7b4fb7233aaa9935d55e42e3e0b21fade52b521
Soma Szolyák <soma.szolyak@gmail.com>
2018-04-23 13:05:14 +0200

    added skeleton classes separated for layers

---
+++ src/main/java/com/codecool/apiwars/controller/WarController.java
@@ -0,0 +1,4 @@
+package com.codecool.apiwars.controller;
+
+public class WarController {
+}
---
+++ src/main/java/com/codecool/apiwars/model/User.java
@@ -0,0 +1,18 @@
+package com.codecool.apiwars.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "`user`")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String psw;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
